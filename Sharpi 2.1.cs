1195b2d9a2b0a09eda219c403d30c4f9d06f1129
TheBlackTown <70762240+TheBlackTown@users.noreply.github.com>
2020-09-12 11:23:23 +0300

    Add files via upload

---
+++ Sharpi 2.1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace icr.List_Value_Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte a = 0;
+            byte b = 0;
+            Int16 c = 0;
+            Int32 d = 0;
+            Int64 e = 0;
+            string s = " ";
+            Exception ex = new Exception();
+            object[] types = { a, b, c, d, e, s, ex };
+
+            foreach (object obj in types)
+            {
+                string type;
+                if (obj.GetType().IsValueType)
+                    type = "Value type";
+                else
+                    type = "Reference Type";
+                Console.WriteLine($"{obj.GetType()}: {type}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
